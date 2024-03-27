486344671901f648203feb1cb0e98e2c5f331d9a
wheele19 <wheele19@gmail.com>
2024-03-26 22:32:59 -0400

    Neural Network

--- handwritten_digits.py
+++ handwritten_digits.py
@@ -1,38 +1,50 @@
-# Import necessary libraries
-import numpy as np
+import os
+os.environ['KERAS_BACKEND'] = 'theano'
+
+import keras
 from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers import Dense, Flatten
-from keras.utils import to_categorical
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.utils import np_utils
 
 # Load the MNIST dataset
-(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-# Preprocess the data
-train_images = train_images.astype('float32') / 255
-test_images = test_images.astype('float32') / 255
+# Reshape the data to [samples][pixels][width][height]
+X_train = X_train.reshape(X_train.shape[0], 28, 28, 1).astype('float32')
+X_test = X_test.reshape(X_test.shape[0], 28, 28, 1).astype('float32')
 
-train_images = np.expand_dims(train_images, axis=-1)
-test_images = np.expand_dims(test_images, axis=-1)
+# Normalize inputs from 0-255 to 0-1
+X_train = X_train / 255
+X_test = X_test / 255
 
-train_labels = to_categorical(train_labels)
-test_labels = to_categorical(test_labels)
+# One hot encode outputs
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+num_classes = y_test.shape[1]
 
-# Define the neural network model
-model = Sequential([
-    Flatten(input_shape=(28, 28, 1)),
-    Dense(128, activation='relu'),
-    Dense(10, activation='softmax')
-])
+# Define the model
+def baseline_model():
+    # Create model
+    model = Sequential()
+    model.add(Conv2D(32, (5, 5), input_shape=(28, 28, 1), activation='relu'))
+    model.add(MaxPooling2D())
+    model.add(Dropout(0.2))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(num_classes, activation='softmax'))
+    
+    # Compile model
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
 
-# Compile the model
-model.compile(optimizer='adam',
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
+# Build the model
+model = baseline_model()
 
-# Train the model
-model.fit(train_images, train_labels, epochs=5, batch_size=64)
+# Fit the model
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200, verbose=2)
 
-# Evaluate the model
-test_loss, test_acc = model.evaluate(test_images, test_labels)
-print('Test accuracy:', test_acc)
+# Final evaluation of the model
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Baseline Error: %.2f%%" % (100-scores[1]*100))
